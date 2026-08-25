9e3e4388af91f5d5e49044c9442ab24c5a690a8a
Nurunnahar nody <nurunnaharnody0011@gmail.com>
2026-08-25 18:18:20 +0200

    first commit

---
+++ contest.js
@@ -0,0 +1,64 @@
+function getMonthName(monthNumber) {
+  if(monthNumber === 1){
+    return "January";
+  }
+    else if(monthNumber === 2){
+    return "February";
+  }
+    else if(monthNumber === 3){
+    return "March";
+}
+else if(monthNumber === 4){
+    return "April";
+}
+else if(monthNumber === 5){
+    return "May";
+}
+else if(monthNumber === 6){
+    return "June";
+}
+else if(monthNumber === 7){
+    return "July";
+}
+else if(monthNumber === 8){
+    return "August";
+}
+else if(monthNumber === 9){
+    return "September";
+}
+else if(monthNumber === 10){
+    return "October";
+}
+else if(monthNumber === 11){
+    return "November";
+}
+else if(monthNumber === 12){
+    return "December";
+}
+}
+
+ // Output: January
+
+function countNumberProperties(numbers) {
+
+   const even = numbers.filter(num => num%2===0);
+   const odd = numbers.filter(num => num% 2!== 0);
+   const negative =numbers.filter(num=> num<0);
+   const positive = numbers.filter(num => num>0);
+return `"{"even":${even.length},"negative":${negative.length},"odd":${odd.length},"positive":${positive.length}`
+
+}
+
+function checkMathOperationsForNine(a, b) {
+    
+  const sum = a+b;
+  const difference = a-b;
+  const product = a*b;
+  const quotient = a/b;
+
+if(sum===9 || difference===9 || product===9|| quotient===9){
+    return "Nine";
+}
+return "Nein";
+}
+
